e3c746a1c8e4f0195186226ce6f3a95a387f5766
Liberxue <Liberxue@users.noreply.github.com>
2017-06-06 11:01:08 +0800

    push code appunzip
    push code appunzip

---
+++ appZip/tests/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following
+// attributes.
+//
+// change them to the information which is associated with the assembly
+// you compile.
+
+[assembly: AssemblyTitle("liberxueZIP")]
+[assembly: AssemblyDescription("liberxueZIP")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("北京华莘智慧教育")]
+[assembly: AssemblyProduct("#ZipLibrary")]
+[assembly: AssemblyCopyright("Copyright 2017 liberxueZIP")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all values by your own or you can build default build and revision
+// numbers with the '*' character (the default):
+
+[assembly: AssemblyVersion("0.85.5.452")]
+[assembly: AssemblyInformationalVersionAttribute("0.85.5")]
+
+// The following attributes specify the key for the sign of your assembly. See the
+// .NET Framework documentation for more information about signing.
+// This is not required, if you don't want signing let these attributes like they're.
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
